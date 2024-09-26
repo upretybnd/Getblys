ed1127602604235a059657e0e4b1983efad7d944
Binod Uprety <meet.upretybnd@gmail.com>
2024-09-26 15:46:49 +0545

    host changes

--- getblys/settings.py
+++ getblys/settings.py
@@ -57,7 +57,9 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:8000',
-    'https://getblys-production.up.railway.app',]
+    'https://getblys-production.up.railway.app',
+    'https://getblys.vercel.app',
+]
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
