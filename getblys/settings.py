bbbefc18068ff143c75146b4277f657d4b7a66f0
Binod Uprety <meet.upretybnd@gmail.com>
2024-09-26 15:19:50 +0545

    Updated ALLOWED_HOSTS for Railway deployment

--- getblys/settings.py
+++ getblys/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4pe-p@2c6375a7hlp+%xmec-bohypv=+k0sc#b$#rne08fvzqr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['getblys-production.up.railway.app',']
 
 
 # Application definition
@@ -57,6 +57,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:8000',
+    'getblys.vercel.app',
 ]
 TEMPLATES = [
     {
